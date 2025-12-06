2b4f7b0a88a9ebb83fb0df6596ef3901818c2e39
mogsetta <v84fnp55jq@privaterelay.appleid.com>
2025-12-06 16:33:24 -0600

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  swcMinify: true,
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client"],
+  },
+};
+
+module.exports = nextConfig;
